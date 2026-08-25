21d32be56391c338741413a397c09966c919f595
mmdaqajan <mohamad.fathi13811@gmail.com>
2026-08-25 13:38:02 +0330

    creating admin pannel for menu and order module

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'created_at')
+    list_filter = ('is_active',)
+    search_fields = ('title',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'price', 'is_available', 'created_at')
+    list_filter = ('is_available', 'category')
+    search_fields = ('title', 'description')
+    list_editable = ('price', 'is_available')  # تغییر سریع قیمت و موجودی مستقیماً از لیست محصولات
+
+    

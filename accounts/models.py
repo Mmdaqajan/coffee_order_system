b55f16f6457e4a32f476edfa0f01b0e068cc4360
mmdaqajan <mohamad.fathi13811@gmail.com>
2026-08-31 17:26:20 +0330

    creating barista models and adding it to admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ("admin", "مدیر"),
+        ("barista", "باریستا"),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", verbose_name="کاربر")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="barista", verbose_name="نقش")
+
+    class Meta:
+        verbose_name = "پروفایل کاربر"
+        verbose_name_plural = "پروفایل‌های کاربران"
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
